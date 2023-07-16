6d5f67b7dbe1334d229bca1ad94b49add0c322cd
GIO E <gioe1996@gmail.com>
2023-07-16 16:17:52 -0500

    images should pre-render now

--- main.js
+++ main.js
@@ -18,46 +18,4 @@ document.querySelectorAll('a').forEach((a) => {
   
 });
 
-// THIS PART OF THE CODE PRE-RENDERS images
-
-window.addEventListener('load', function() {
-  // Find all of the links
-  const links = document.querySelectorAll('a');
-
-  links.forEach((a) => {
-    // Get the URL of the image for this link
-    const imageUrl = a.getAttribute('data-preview');
-
-    // If this link has an associated image...
-    if(imageUrl) {
-      // ... create a new image element
-      const img = document.createElement('img');
-
-      // Set the source of the image element to the URL
-      img.src = imageUrl;
-
-      // Hide the image
-      img.style.display = 'none';
-
-      // Append the image element to the body
-      // (this will start loading the image in the background)
-      document.body.appendChild(img);
-
-      // When the link is hovered over...
-      a.addEventListener('mouseover', function(e) {
-        // ... replace the image in the preview element with the preloaded image
-        document.getElementById('preview').innerHTML = '';
-        document.getElementById('preview').appendChild(img);
-
-        // Show the preview box
-        document.getElementById('preview').style.display = 'block';
-      });
-
-      // When the mouse leaves the link...
-      a.addEventListener('mouseout', function(e) {
-        // ... hide the preview box
-        document.getElementById('preview').style.display = 'none';
-      });
-    }
-  });
-});
+// THIS PART OF THE CODE PRE-RENDERS images// Load all images at the start
