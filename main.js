50bade17cd57c754bc78a537d837acb14cb6865d
GIO E <gioe1996@gmail.com>
2023-07-16 14:46:30 -0500

    the best table format

--- main.js
+++ main.js
@@ -15,4 +15,23 @@ document.querySelectorAll('a').forEach((a) => {
     // remove the preview when the mouse leaves the link
     document.getElementById('preview').innerHTML = '';
   });
+  function filterTable() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("filterInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("skillsTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];  //Changed index from 0 to 1
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+  
 });
