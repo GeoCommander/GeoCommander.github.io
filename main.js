44498ce0429ee9db500e3dd7ff2c4b5158d2bd98
GIO E <gioe1996@gmail.com>
2023-07-16 11:53:22 -0500

    table format should be created and first link should work

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 document.querySelectorAll('a').forEach((a) => {
-    a.addEventListener('mouseover', function(e) {
-      const imageUrl = this.getAttribute('data-preview');
-      if(imageUrl) {
-        // load the corresponding image into the preview element
-        document.getElementById('preview').innerHTML = `<img src="${imageUrl}" alt="Preview">`;
-        // show the preview box
-        document.getElementById('preview').style.display = 'block';
-      }
-    });
-  
-    a.addEventListener('mouseout', function(e) {
-      // hide the preview box
-      document.getElementById('preview').style.display = 'none';
-      // remove the preview when the mouse leaves the link
-      document.getElementById('preview').innerHTML = '';
-    });
-  });  
+  a.addEventListener('mouseover', function(e) {
+    const imageUrl = this.getAttribute('data-preview');
+    if(imageUrl) {
+      // load the corresponding image into the preview element
+      document.getElementById('preview').innerHTML = `<img src="${imageUrl}" alt="Preview">`;
+      // show the preview box
+      document.getElementById('preview').style.display = 'block';
+    }
+  });
+
+  a.addEventListener('mouseout', function(e) {
+    // hide the preview box
+    document.getElementById('preview').style.display = 'none';
+    // remove the preview when the mouse leaves the link
+    document.getElementById('preview').innerHTML = '';
+  });
+});
